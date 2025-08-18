8998bea4678c608d6f6352bddddc1a02fb291169
Jay Mehta <69752445+star-warrior@users.noreply.github.com>
2025-08-18 20:46:14 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl text-red-400 font-bold leading-relaxed">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
