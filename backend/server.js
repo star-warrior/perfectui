ac35dbdf61185fc540c856fa11179fedd1819fa4
Jay Mehta <69752445+star-warrior@users.noreply.github.com>
2025-09-21 13:18:24 +0530

    New Server on netlify

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = e();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: ["perfect-ui.netlify.app", "http://localhost:5173"],
+    origin: [process.env.FRONTEND, "http://localhost:5173"],
     credentials: true,
     allowedHeaders: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
